258bf01a2b8dc924dd44be5329d75e9e9a1b797e
SATYAM MAURY <satyam.maury5963@gmail.com>
2026-08-31 17:21:39 +0530

    Time: 0 ms (100%), Space: 8 MB (63.4%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,18 +1,35 @@
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) {
+//         vector<int> arr;
+//         for(int i=1;i<=n;i++)
+//         {
+//             arr.push_back(i);
+//         }
+//         int i=0;
+//         while(arr.size()>1)
+//         {
+//             int idx=(i+k-1)%arr.size();
+//             arr.erase(arr.begin()+idx);
+//             i=idx;
+//         }
+//         return arr[0];
+//     }
+// };
 class Solution {
 public:
+    int find(int n, int k) {
+        if (n == 1)
+            return 0;
+
+        int ind = find(n - 1, k);
+        ind = (ind + k) % n;
+
+        return ind;  
+    }
+
     int findTheWinner(int n, int k) {
-        vector<int> arr;
-        for(int i=1;i<=n;i++)
-        {
-            arr.push_back(i);
-        }
-        int i=0;
-        while(arr.size()>1)
-        {
-            int idx=(i+k-1)%arr.size();
-            arr.erase(arr.begin()+idx);
-            i=idx;
-        }
-        return arr[0];
+        int result = find(n, k); 
+        return result + 1;
     }
 };
