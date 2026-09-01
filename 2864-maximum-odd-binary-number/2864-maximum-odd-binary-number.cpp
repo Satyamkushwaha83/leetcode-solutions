e6c65f3042be0380c6082117697b25cf969ac9aa
SATYAM MAURY <satyam.maury5963@gmail.com>
2026-09-01 21:55:39 +0530

    Time: 6 ms (3.58%), Space: 11.2 MB (5.26%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+    //   string ans = "";
+    string ans(1, s[0]);
+        for(int i=1; i <s.size();i++)
+        {
+            if(s[i]=='1')
+            {
+                ans=s[i]+ans;
+            }
+            else
+            {
+                ans=ans+s[i];
+            }
+
+        }
+        int pos = ans.find('1');
+
+        ans.erase(pos, 1);
+        ans += '1';
+        return ans;
+
+    }
+};
